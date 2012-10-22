df3c26a82936a107c8799eaae73032d333ccd409
benamaramir <benamaramir@gmail.com>
2012-10-22 12:55:15 +0200

    third commit

--- EGit github Tutorial/src/com/Artesis/Github/EersteKlasse.java
+++ EGit github Tutorial/src/com/Artesis/Github/EersteKlasse.java
@@ -3,5 +3,6 @@ package com.Artesis.Github;
 public class EersteKlasse {
 	/* dit is de eerste test voor github op te zetten */
 	/* dit is de tweede test */
+	/* dus eerst opslaan, project commit, ssh pushen*/
 
 }
