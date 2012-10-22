dd491467b72dc2020848d060ce2508097994c68f
benamaramir <benamaramir@gmail.com>
2012-10-22 13:58:00 +0300

    Update EGit github Tutorial/src/com/Artesis/Github/EersteKlasse.java

--- EGit github Tutorial/src/com/Artesis/Github/EersteKlasse.java
+++ EGit github Tutorial/src/com/Artesis/Github/EersteKlasse.java
@@ -4,5 +4,6 @@ public class EersteKlasse {
 	/* dit is de eerste test voor github op te zetten */
 	/* dit is de tweede test */
 	/* dus eerst opslaan, project commit, ssh pushen*/
+	/* dit is nu een aanpassing in github die ik in eclipse wil terugzien*/
 
 }
