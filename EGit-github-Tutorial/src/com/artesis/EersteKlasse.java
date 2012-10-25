f8bdb9ee5ced48e402364172fd61304db182ff1a
benamaramir <benamaramir@gmail.com>
2012-10-25 19:31:42 +0300

    Update EGit-github-Tutorial/src/com/artesis/EersteKlasse.java

--- EGit-github-Tutorial/src/com/artesis/EersteKlasse.java
+++ EGit-github-Tutorial/src/com/artesis/EersteKlasse.java
@@ -4,4 +4,5 @@ public class EersteKlasse {
 /* eerste regel commentaar */
 	/*tweede test*/
 	/*derde test*/
+/*dit is een wijziging die in de browser gebeurd is en eens zien of deze in eclipse werkt*/
 }
